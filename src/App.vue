4e148448959b60711295be520b36300256ec870b
salvadorperezm <salvadorperezm115@gmail.com>
2023-02-25 17:50:28 -0600

    removing footer from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <h1>Hello World</h1>
-  <the-footer />
 </template>
 
 <script>
-import TheFooter from './components/TheFooter.vue';
-export default {
-  components: {
-    'the-footer': TheFooter
-  }
-}
+
 </script>
 
 <style scoped></style>
