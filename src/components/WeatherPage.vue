1c28ee61f34c5deb8813f6a39e5bdaa70cad352b
salvadorperezm <salvadorperezm115@gmail.com>
2023-02-27 12:25:21 -0600

    top section basic design done

--- src/components/WeatherPage.vue
+++ src/components/WeatherPage.vue
@@ -2,14 +2,22 @@
     <page-layout>
         <template v-slot:default>
             <section class="content__section">
-                <div class="section section__top">
-                    <div class="weather__actions">
-                        <div class="weather__current-location">
-                            <font-awesome-icon icon="fa-solid fa-location-dot" />
-                            <p>{{ currentLocation }}</p>
+                <div class="section section__top-container">
+                    <div class="section__top">
+                        <div class="weather__actions">
+                            <div class="weather__current-location">
+                                <font-awesome-icon icon="fa-solid fa-location-dot" class="current-location__icon" />
+                                <p class="current-location__text">{{ currentLocation }}</p>
+                            </div>
+                            <font-awesome-icon icon="fa-solid fa-arrows-rotate" class="weather__reload" />
                         </div>
-                        <div class="weather__reload">
-                            <font-awesome-icon icon="fa-solid fa-arrows-rotate" />
+                        <div class="weather__information">
+                            <div class="information__text">
+                                <h1 class="information__current-temperature">{{ currentTemperature }}&#176;C</h1>
+                                <p class="information__feels-like">Feels like {{ currentFeelsLike }}&#176;C</p>
+                                <p class="information__weather-type">{{ currentWeatherType }}</p>
+                            </div>
+                            <img src="../assets/dummy-weather.svg" alt="Dummy Weather" class="information__icon">
                         </div>
                     </div>
                 </div>
@@ -29,8 +37,10 @@ export default {
     },
     data() {
         return {
-            currentTemperate: '32',
-            currentLocation: 'Managua'
+            currentTemperature: '32',
+            currentLocation: 'Managua',
+            currentFeelsLike: '32',
+            currentWeatherType: 'Scattered clouds'
         }
     }
 }
@@ -48,8 +58,19 @@ export default {
     flex: 1;
 }
 
-.section__top {
+.section__top-container {
     background-color: #5d9be5;
+    display: flex;
+    justify-content: center;
+}
+
+.section__top {
+    flex: 1;
+    max-width: 1200px;
+    height: 100%;
+    color: #ffffff;
+    display: flex;
+    flex-direction: column;
 }
 
 .section__bottom {
@@ -70,14 +91,61 @@ export default {
 }
 
 .weather__current-location {
-
     gap: 8px;
+    padding: 8px;
+}
+
+.current-location__icon {
+    width: 21px;
+}
+
+
+.weather__reload {
+    padding: 8px;
+}
+
+.weather__information {
+    justify-self: center;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 32px 32px;
+}
+
+.information__current-temperature {
+    font-size: 64px;
+    font-weight: 400;
+    margin-block-end: 16px;
+}
+
+
+
+.information__feels-like {
+    margin-block-end: 8px;
+}
+
+.information__icon {
+    width: 192px;
 }
 
 @media screen and (max-width: 1024px) {
     .section__bottom {
         border-top-right-radius: 16px;
         border-top-left-radius: 16px;
+    }
+
+    .current-location__text {
+        font-size: 14px;
+    }
+
+    .information__feels-like,
+    .information__weather-type {
+        font-size: 12.25px;
+    }
+
+    .information__icon {
+        width: 96px;
     }
 }
 </style>
